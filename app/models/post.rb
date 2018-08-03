d53da843ddfb5cfbafbfeae4005cca0b9363cb46
Aoife McGann <aoifemcgann@Sarahs-MacBook-Pro.local>
2018-08-03 10:31:28 +0100

    initial commit

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+	has_attached_file :picture, styles:{ medium:"300x300#",thumb:"100x100#"}
+
+	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+end
